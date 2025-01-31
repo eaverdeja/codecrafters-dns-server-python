cd90f32bcb8ba61bb077eed9ae2433a257ab2236
eaverdeja <eaverdeja@gmail.com>
2025-01-31 19:09:36 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,24 +2,23 @@ import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-    #
-    # udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # udp_socket.bind(("127.0.0.1", 2053))
-    #
-    # while True:
-    #     try:
-    #         buf, source = udp_socket.recvfrom(512)
-    #
-    #         response = b""
-    #
-    #         udp_socket.sendto(response, source)
-    #     except Exception as e:
-    #         print(f"Error receiving data: {e}")
-    #         break
+    # AF_INET means we're using IPv4
+    # SOCK_DGRAM specifies UDP protocol (as opposed to TCP which would be SOCK_STREAM)
+    # DGRAM stands for (User) DataGram (Protocol), or UDP for short ;)
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    # Note: Standard DNS uses port 53, but that usually requires root privileges
+    udp_socket.bind(("127.0.0.1", 2053))
+    
+    while True:
+        try:
+            buf, source = udp_socket.recvfrom(512)
+            response = b""
+    
+            udp_socket.sendto(response, source)
+        except Exception as e:
+            print(f"Error receiving data: {e}")
+            break
 
 
 if __name__ == "__main__":
