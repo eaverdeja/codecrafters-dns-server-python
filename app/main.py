4f8b4e881c66ee5a7b9ac9d00a2a2cbc0ae563d9
eaverdeja <eaverdeja@gmail.com>
2025-02-02 08:48:05 -0300

    WIP - forwarding queries

--- app/main.py
+++ app/main.py
@@ -1,23 +1,15 @@
 import socket
+from argparse import ArgumentParser
 
 from .dns import DNSMessage
 
 
-def main():
-    # AF_INET means we're using IPv4
-    # SOCK_DGRAM specifies UDP protocol (as opposed to TCP which would be SOCK_STREAM)
-    # DGRAM stands for (User) DataGram (Protocol), or UDP for short ;)
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    # Note: Standard DNS uses port 53, but that usually requires root privileges
-    udp_socket.bind(("127.0.0.1", 2053))
-
+def _run_server(udp_socket: socket.SocketType):
     while True:
         try:
             buf, source = udp_socket.recvfrom(512)
-            # The DNS packet header is always 12 bytes in length
-            header = buf[:12]
 
+            header = buf[: DNSMessage.HEADER_SIZE]
             query_header = DNSMessage.parse_header(header)
 
             offset = DNSMessage.HEADER_SIZE
@@ -49,6 +41,54 @@ def main():
         except Exception as e:
             print(f"Error receiving data: {e}")
             break
+
+
+def _run_forwarding_server(udp_socket: socket.SocketType, address: str, port: int):
+    while True:
+        # Receive
+        buf, source = udp_socket.recvfrom(512)
+
+        # Split questions
+        ...
+
+        # Forward
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        client_socket.sendto(buf, (address, port))
+
+        # Merge responses
+        ...
+
+        # Respond
+        server_response = client_socket.recv(512)
+        udp_socket.sendto(server_response, source)
+
+
+def main():
+    parser = ArgumentParser(description="Simple DNS server")
+    parser.add_argument(
+        "--resolver", type=str, help="The address of the resolver DNS server"
+    )
+    parser.add_argument(
+        # Note: Standard DNS uses port 53, but that usually requires root privileges
+        "--port",
+        type=str,
+        help="The port of the DNS server",
+        default=2053,
+    )
+    args = parser.parse_args()
+
+    # AF_INET means we're using IPv4
+    # SOCK_DGRAM specifies UDP protocol (as opposed to TCP which would be SOCK_STREAM)
+    # DGRAM stands for (User) DataGram (Protocol), or UDP for short ;)
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    udp_socket.bind(("127.0.0.1", int(args.port)))
+
+    if args.resolver:
+        address, port = args.resolver.split(":")
+        _run_forwarding_server(udp_socket, address, int(port))
+    else:
+        _run_server(udp_socket)
 
 
 if __name__ == "__main__":
